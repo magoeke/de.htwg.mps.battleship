3a7dc1cc0f33f58e5b037edf1d6a4486a350819f
magoeke <magoeke@htwg-konstanz.de>
2016-10-21 19:48:31 +0200

    Added fire method.

--- src/main/scala/de/htwg/mps/battleship/model/Gamefield.scala
+++ src/main/scala/de/htwg/mps/battleship/model/Gamefield.scala
@@ -1,6 +1,16 @@
 package de.htwg.mps.battleship.model
 
-case class Gamefield(size: Int, field: Array[Array[Field]], ships: List[Ship]) {
+case class Gamefield(field: Array[Array[Field]], ships: List[Ship]) {
   //  var field = Array.fill[Field](size, size) { Field(false) }
-  override def toString: String = "Gamefield: " + size
+  override def toString: String = "Gamefield: "
+  
+  def fire(x: Int, y: Int) = {
+    val newF = Field(true)
+    val oldF = field(x)(y)
+    val newField = field.map(_.map(compareFields(_, field(x)(y), newF)))
+    val newShips = ships.map(ship => ship.copy(ship.pos.map(compareFields(_, oldF, newF))))
+    copy(newField, newShips)
+  }
+  
+  def compareFields(f1: Field, f2:Field, f3:Field) = if(f1 eq f2) f3 else f1
 }
