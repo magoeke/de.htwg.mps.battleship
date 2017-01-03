8c508d9f2566325148a94d74f0622f7a121bce39
magoeke <magoeke@htwg-konstanz.de>
2017-01-03 22:13:13 +0100

    Added filter opportunity.

--- src/main/scala/de/htwg/mps/battleship/controller/ControllerActor.scala
+++ src/main/scala/de/htwg/mps/battleship/controller/ControllerActor.scala
@@ -5,19 +5,23 @@ import de.htwg.mps.battleship.controller.command._
 import de.htwg.mps.battleship.model.IPlayer
 
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
 
+case class RegisterUI(player: String)
 case object RegisterUI
 case object DeregisterUI
 
 class ControllerActor(val players: List[IPlayer]) extends Actor {
   val controller: IBattleshipController = new BattleshipController(players)
   val userInterfaces = new ListBuffer[ActorRef]()
+  val actorToUser = Map[ActorRef, String]()
 
   override def receive: Receive = {
-    case RegisterUI => userInterfaces += sender(); sender() ! createUpdateUI()
-    case DeregisterUI => userInterfaces -= sender()
+    case RegisterUI => userInterfaces += sender; sender() ! createUpdateUI(sender())
+    case RegisterUI(player) => register(player, sender()); sender() ! createUpdateUI(sender())
+    case DeregisterUI => deregister(sender())
     case command: Command => controller.handleCommand(command) match {
-      case true => userInterfaces.foreach(_ ! createUpdateUI())
+      case true => userInterfaces.foreach(actor => actor ! createUpdateUI(actor))
       case _ => {
         val winner = controller.getWinner
         if(winner.isDefined) { userInterfaces.foreach(_ ! winner.get.name) }
@@ -29,8 +33,16 @@ class ControllerActor(val players: List[IPlayer]) extends Actor {
     case Player => sender() ! APlayer(controller.currentPlayer.name)
   }
 
-  private def createUpdateUI() = {
-    UpdateUI(controller.currentPlayer.name, controller.setableShips.map(_.size), controller.boardsView)
+  private def register(player: String, sender: ActorRef) = {
+    userInterfaces += sender
+    actorToUser += (sender -> player)
+  }
+
+  private def deregister(sender: ActorRef) = userInterfaces -= sender; actorToUser -= sender
+
+  private def createUpdateUI(actor: ActorRef) = {
+    val player: String = actorToUser.getOrElse(actor, controller.currentPlayer.name)
+    UpdateUI(controller.currentPlayer.name, controller.setableShips.map(_.size), controller.boardsView(player))
   }
 }
 
