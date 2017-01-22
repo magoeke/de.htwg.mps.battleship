b381c9d65b1d5c590bca65b0cead1bac06614332
mGrondzi <manuel.grondziel@htwg-konstanz.de>
2017-01-22 21:50:26 +0100

    Modeltests

--- src/test/scala/de/htwg/mps/battleship/controller/model/ShipSpec.scala
+++ src/test/scala/de/htwg/mps/battleship/controller/model/ShipSpec.scala
@@ -4,28 +4,32 @@ import scala.de.htwg.mps.battleship.model.impl.{Field, Ship}
 
 
 class ShipSpec extends ModelSpec {
-  "A new Ship" should {
+  val shipSize = 2
+  "A not initialized Ship with Size " +shipSize should {
     //Ship with with Length and no position
-    val ship = Ship(2)
+    val ship = Ship(shipSize)
     "should have a size" in {
-      ship.size should be (2)
+      ship.size should be (shipSize)
     }
     "should have a empty Pos" in {
       ship.pos should be (List())
     }
-    "should have a satus if inizialized" in {
+    "should have a status if inizialized" in {
       ship.initialized should be (false)
     }
+  }
 
-    val shipPos =List(new Field(false), new Field(false), new Field(false))
-    val shipWithPos = Ship(shipPos)
-    "should have a size" in {
-      shipWithPos.size should be (3)
+  "A initialized Ship" should {
+    //Ship with with Length and no position
+    val shipPos =List(new Field(false), new Field(false))
+    val shipWithPos = new Ship(shipPos,shipPos.length,true)
+    "should have a size" in {5
+      shipWithPos.size should be (shipSize)
     }
-    "should have a empty Pos" in {
+    "should have a Pos" in {
       shipWithPos.pos should be (shipPos)
     }
-    "should have a satus if inizialized" in {
+    "should have a status if inizialized" in {
       shipWithPos.initialized should be (true)
     }
   }
